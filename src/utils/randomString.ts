d4733607c44f5a3bc8e7a0f376bfa4447b87b739
Petr Tripolsky <tripolskypetr@gmail.com>
2024-12-26 17:19:47 +0500

    deps-cleanup

--- src/utils/randomString.ts
+++ src/utils/randomString.ts
@@ -1,4 +1,10 @@
-import { v4 as uuid } from 'uuid';
+const str = () => ('00000000000000000' + (Math.random() * 0xffffffffffffffff).toString(16)).slice(-16);
+
+const uuid = () => {
+  const a = str();
+  const b = str();
+  return a.slice(0, 8) + '-' + a.slice(8, 12) + '-4' + a.slice(13) + '-a' + b.slice(1, 4) + '-' + b.slice(4);
+};
 
 /**
  * Generates a random string using the UUID library.
