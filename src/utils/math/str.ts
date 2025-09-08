8acbbf1884bd09c5a324ea88041c4ee3647eb0d0
Petr Tripolsky <tripolskypetr@gmail.com>
2025-09-08 17:01:53 +0500

    inc

--- src/utils/math/str.ts
+++ src/utils/math/str.ts
@@ -43,4 +43,13 @@ str.semicolon =  (...arr: (number | string | string[] | null)[] | (number | stri
     return join(...arr).join(";");
 };
 
+/**
+ * Joins an array of strings or arrays of strings into a markdown table row format.
+ * Each element is separated by a pipe character and spaces.
+ * For example, inputting ["Name", "Age", "Location"] would return "| Name | Age | Location |".
+ */
+str.table = (...arr: (number | string | string[] | null)[] | (number | string | string[] | null)[][]): string => {
+    return `| ${join(...arr).join(" | ")} |`;
+};
+
 export default str;
--- types.d.ts
+++ types.d.ts
@@ -1739,6 +1739,12 @@ declare const str: {
      * Joins an array of strings or arrays of strings into a single string, with each element separated by a semicolon.
      */
     semicolon(...arr: (number | string | string[] | null)[] | (number | string | string[] | null)[][]): string;
+    /**
+     * Joins an array of strings or arrays of strings into a markdown table row format.
+     * Each element is separated by a pipe character and spaces.
+     * For example, inputting ["Name", "Age", "Location"] would return "| Name | Age | Location |".
+     */
+    table(...arr: (number | string | string[] | null)[] | (number | string | string[] | null)[][]): string;
 };
 
 /**
